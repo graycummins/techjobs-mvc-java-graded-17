c262849b29d198607a140c1334687229156e9513
graycummins <>
2023-10-23 02:42:41 -0500

    commenting done

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -29,17 +29,25 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
-    public String displaySearchResults(Model model, @RequestParam(value="", required=false) String searchType, @RequestParam(value="", required=false) String searchTerm)
+    public String displaySearchResults(Model model, @RequestParam(value="", required = false) String searchType, @RequestParam(value="", required = false) String searchTerm)
     {
+        //displays all jobs that match the search requirements
+        //makes sure that even if the user enters nothing, it will still display some results
+
+        //create new array to hold all the jobs that match the search criteria
         ArrayList<Job> jobs;
+
+        //if the user searched all, entered nothing, or put a blank line the output will display all the jobs
         if(searchTerm.toUpperCase() == "ALL" || searchTerm.isBlank() || searchTerm.isEmpty())
         {
             jobs = JobData.findAll();
         }
         else
+        //find the job that matches the entered criteria
         {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
+        //add all the searched criteria
         model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
         model.addAttribute("searchType", columnChoices);
         model.addAttribute("jobs", jobs);
